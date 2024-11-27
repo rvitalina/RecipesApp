484270f2be049c94725069234594d9ae5f2ebb58
Vitalina Rukus <v.rukus>
2024-11-27 22:57:41 +0300

    feat: create pool for db

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,13 @@ const Pool = require('pg').Pool;
 const pool = new Pool ({
     user: "postgres", 
     password: "helloworld0905", 
-    host: "localhost", 
+    host: "localhost",  
     port: 5123, 
     database: "recipesdb"
 })
+
+// const Sequelize = require('sequelize');
+
+// module.exports = new Sequelize()
+
+module.exports = pool;
