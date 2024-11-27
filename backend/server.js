235a32828eacc42317dc4cbf7ebc5414830cff35
Vitalina Rukus <v.rukus>
2024-11-27 22:32:05 +0300

    feat: create server.js file to add port and frameworks

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv").config();
+require("dotenv").config();
+const pool = require("./db");
+const cors = require("cors");
+const router = require("./routes/recipe")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.use("/recipe", require("./routes/recipe"));
+app.use(express.json());
+app.use(cors());
 
+app.use("/recipe", router);
 
 app.listen(PORT, (err) => {
   console.log(`App is listening on port ${PORT}`);
 });
+
