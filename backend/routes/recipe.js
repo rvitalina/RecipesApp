21de390f56d728efc127314efd194711c61f0715
Vitalina Rukus <v.rukus>
2024-12-19 01:03:27 +0300

    fix: change routes for recipe

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe } = require("../controller/recipe");
+const { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe, upload } = require("../controller/recipe");
+const verifyToken = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", getRecipes); // get all recipes
 router.get("/:id", getRecipe); // get recipe by id
-router.post("/", addRecipe); // add recipe
-router.put("/:id", editRecipe); // edit recipe by id
+router.post("/", upload.single('file'), verifyToken, addRecipe); // add recipe
+router.put("/:id", upload.single('file'), editRecipe); // edit recipe by id
 router.delete("/:id", deleteRecipe); // delete recipe by id
 
 module.exports = router;
