d43d6d579e913c996f8e269cb1439c66e20fe57d
Vitalina Rukus <v.rukus>
2024-12-19 01:07:03 +0300

    fix: change names of header parts

--- frontend/recipes_app/src/components/NavBar.jsx
+++ frontend/recipes_app/src/components/NavBar.jsx
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from 'react'
-// import Modal from './Modal'
+import Modal from './Modal'
 // import InputForm from './InputForm'
 import { NavLink } from 'react-router-dom'
+import InputForm from './InputForm'
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  let token = localStorage.getItem("token")
+  const [isLogin, setIsLogin] = useState(token ? false : true)
+
+  let user=JSON.parse(localStorage.getItem("user"))
+
+  useEffect(() => {
+    setIsLogin(token ? false : true)
+  }, [token])
+
+  const checkLogin = () => {
+    if(token) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      setIsLogin(true)
+    }
+    setIsOpen(true)
+  }
 
   return (
     <>
-        <header>
-            <h2>Yummy App</h2>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink>My Recipe</NavLink></li>
-                <li><NavLink>Favourites</NavLink></li>
-                <li><NavLink>Log in</NavLink></li>
-                {/* <li>{ (isLogin)? "Login": "Logout" }{user?.email ? `(${user?.email})` : ""}</p></li> */}
-            </ul>
-        </header>
-       {/* { (isOpen) && <Modal onClose={()=>setIsOpen(false)}><InputForm setIsOpen={()=>setIsOpen(false)}/></Modal>} */}
+      <header>
+        <h2>Yummy App</h2>
+        <ul>
+          <li><NavLink to="/">Главная</NavLink></li>
+          <li onClick={() => isLogin && setIsOpen(true)}><NavLink to={!isLogin ? "/myRecipe" : "/"}>Мои рецепты</NavLink></li>
+          <li onClick={() => isLogin && setIsOpen(true)}><NavLink to={!isLogin ? "/favRecipe" : "/"}>Любимые</NavLink></li>
+          <li onClick={checkLogin}>
+            <p className="login">{ (isLogin) ? "Вход" : "Выход "}{user?.email ? `(${user?.email})` : ""}</p>
+          </li>
+        </ul>
+      </header>
+      {(isOpen) && <Modal onClose={() => setIsOpen(false)}><InputForm setIsOpen={() => setIsOpen(false)} /></Modal>}
     </>
   )
 }
